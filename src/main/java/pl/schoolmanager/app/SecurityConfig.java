a949ab6acbc413f5eba239b89e5cc3e42851c31c
Lukasz.Budyn <lukasz-budyn@wp.pl>
2018-01-16 20:21:12 +0100

    teacher and mark access to admin only - temporary

--- src/main/java/pl/schoolmanager/app/SecurityConfig.java
+++ src/main/java/pl/schoolmanager/app/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/login", "/register").permitAll()
-			.antMatchers("/teacher/**")
+			.antMatchers("/teacher/**", "/mark/**")
 			.hasRole("ADMIN").anyRequest().authenticated()
 			.and()
 			.formLogin().loginPage("/login").permitAll()
