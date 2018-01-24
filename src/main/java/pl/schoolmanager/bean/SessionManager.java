fdac524f8c69b8541ee146afe088a612b341fe18
przemyslaw <przemyslawkonik@gmail.com>
2018-01-24 20:10:24 +0100

    new method in SessionManager that returns logged user

--- src/main/java/pl/schoolmanager/bean/SessionManager.java
+++ src/main/java/pl/schoolmanager/bean/SessionManager.java
@@ -2,16 +2,28 @@ package pl.schoolmanager.bean;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import pl.schoolmanager.entity.User;
+import pl.schoolmanager.repository.UserRepository;
 
 @Component
 public class SessionManager {
+
+    @Autowired
+    private UserRepository userRepository;
 
 	public static HttpSession session() {
 		ServletRequestAttributes attr = 
 				(ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
 		return attr.getRequest().getSession(true);
 	}
+
+    public User loggedUser() {
+        return userRepository.findOneByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+
 }
