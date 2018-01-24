e04364041de1714d74cb500d04e77ab6d39515bc
przemyslaw <przemyslawkonik@gmail.com>
2018-01-24 20:30:33 +0100

    remove test mapping

--- src/main/java/pl/schoolmanager/controller/HomeController.java
+++ src/main/java/pl/schoolmanager/controller/HomeController.java
@@ -1,22 +1,19 @@
 package pl.schoolmanager.controller;
 
-import javax.transaction.Transactional;
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import pl.schoolmanager.entity.User;
 import pl.schoolmanager.entity.UserRole;
 import pl.schoolmanager.repository.UserRepository;
 import pl.schoolmanager.repository.UserRoleRepository;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 @Controller("/")
 public class HomeController {
@@ -71,14 +68,6 @@ public class HomeController {
 	@GetMapping("403")
 	public String accessDenied() {
 		return "errors/accessDenied";
-	}
-
-	// test mapping
-	@GetMapping("test")
-	@ResponseBody
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public String testAccessDenied() {
-		return "You managed to get here";
 	}
 
 }
