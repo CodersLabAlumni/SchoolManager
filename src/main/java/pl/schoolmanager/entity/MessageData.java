343ded2885fc47f51bfc26485e6f3ca8e8403961
przemyslaw <przemyslawkonik@gmail.com>
2018-01-25 13:06:08 +0100

    create MessageData entity, update Message entity, add relation

---
+++ src/main/java/pl/schoolmanager/entity/MessageData.java
@@ -0,0 +1,55 @@
+package pl.schoolmanager.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "message_data")
+public class MessageData {
+
+    @Id
+    private long id;
+
+    private String receiverDescription;
+
+    private String senderDescription;
+
+    @OneToOne
+    @JoinColumn(name = "message_id")
+    @MapsId
+    private Message message;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getReceiverDescription() {
+        return receiverDescription;
+    }
+
+    public void setReceiverDescription(String receiverDescription) {
+        this.receiverDescription = receiverDescription;
+    }
+
+    public String getSenderDescription() {
+        return senderDescription;
+    }
+
+    public void setSenderDescription(String senderDescription) {
+        this.senderDescription = senderDescription;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        setReceiverDescription(message.getReceiver().getUsername() + "<" + message.getReceiver().getEmail() + ">");
+        setSenderDescription(message.getSender().getEmail());
+        this.message = message;
+    }
+
+}
