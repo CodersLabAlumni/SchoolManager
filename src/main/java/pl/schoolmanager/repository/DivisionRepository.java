aaaa62847ed9f814a24fa71655f2343e18ba7f11
mateuszlichon <mateusz.lichon@gmail.com>
2018-01-25 18:54:14 +0100

    limits teachers and divisions to certain schools in school admin profile

--- src/main/java/pl/schoolmanager/repository/DivisionRepository.java
+++ src/main/java/pl/schoolmanager/repository/DivisionRepository.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pl.schoolmanager.entity.Division;
+import pl.schoolmanager.entity.School;
 
 public interface DivisionRepository extends JpaRepository<Division, Long> {
 
 	List<Division> findAllBySchoolIdIsNull();
 
 	List<Division> findAllBySchoolId(long schoolId);
+
+	List<Division> findAllBySchool(School school);
 
 }
--- src/main/java/pl/schoolmanager/repository/TeacherRepository.java
+++ src/main/java/pl/schoolmanager/repository/TeacherRepository.java
@@ -1,9 +1,11 @@
 package pl.schoolmanager.repository;
 
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import pl.schoolmanager.entity.School;
 import pl.schoolmanager.entity.Teacher;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
@@ -11,5 +13,7 @@ public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 	Set<Teacher> findAllBySchoolId(long schoolId);
 
 	Set<Teacher> findAllBySchoolIdIsNullOrSchoolIdIsNot(long schoolId);
+
+	List<Teacher> findAllBySchool(School school);
 
 }
