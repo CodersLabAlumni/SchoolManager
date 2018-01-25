eca343d5cf3c78ac329d0b233817c9657c491099
mateuszlichon <mateusz.lichon@gmail.com>
2018-01-25 18:58:50 +0100

    cleans school admin controller

--- src/main/java/pl/schoolmanager/controller/SchoolAdminController.java
+++ src/main/java/pl/schoolmanager/controller/SchoolAdminController.java
@@ -54,26 +54,7 @@ public class SchoolAdminController {
 	private MessageRepository messageRepository;
 
 
-/*	@GetMapping("/all")
-	public String all(Model m) {
-		return "teacher/all_teachers";
-	}
 
-	// CREATE
-	@GetMapping("/create")
-	public String createTeacher(Model m) {
-		m.addAttribute("teacher", new Teacher());
-		return "teacher/new_teacher";
-	}
-
-	@PostMapping("/create")
-	public String createTeacherPost(@Valid @ModelAttribute Teacher teacher, BindingResult bindingResult, Model m) {
-		if (bindingResult.hasErrors()) {
-			return "teacher/new_teacher";
-		}
-		this.teacherRepository.save(teacher);
-		return "index";
-	}*/
 
 	// Make new schoolAdmin automatically creating new user role
 	@GetMapping("/userNewSchoolAdmin")
@@ -137,68 +118,6 @@ public class SchoolAdminController {
 		s.setAttribute("thisSchool", thisSchoolAdmin.getSchool());
 		return "redirect:/schoolAdminView/";
 	}
-
-/*	// READ
-	@GetMapping("/view/{teacherId}")
-	public String viewTeacher(Model m, @PathVariable long teacherId) {
-		Teacher teacher = this.teacherRepository.findOne(teacherId);
-		m.addAttribute("teacher", teacher);
-		return "teacher/show_teacher";
-	}
-
-	// UPDATE
-	@GetMapping("/update/{teacherId}")
-	public String updateTeacher(Model m, @PathVariable long teacherId) {
-		Teacher teacher = this.teacherRepository.findOne(teacherId);
-		m.addAttribute("teacher", teacher);
-		return "teacher/edit_teacher";
-	}
-
-	@PostMapping("/update/{teacherId}")
-	public String updateTeacherPost(@Valid @ModelAttribute Teacher teacher, BindingResult bindingResult,
-			@PathVariable long teacherId) {
-		if (bindingResult.hasErrors()) {
-			return "teacher/edit_teacher";
-		}
-		teacher.setId(teacherId);
-		this.teacherRepository.save(teacher);
-		return "index";
-	}
-
-	// DELETE
-	@GetMapping("/delete/{teacherId}")
-	public String deleteTeacher(@PathVariable long teacherId) {
-		this.teacherRepository.delete(teacherId);
-		return "index";
-	}
-
-	// SHOW ALL
-	@ModelAttribute("availableTeachers")
-	public List<Teacher> getTeachers() {
-		return this.teacherRepository.findAll();
-	}
-
-	// ADD SUBJECT TO TEACHER
-	@GetMapping("/addSubject/{teacherId}")
-	public String addSubject(Model m, @PathVariable long teacherId) {
-		Teacher teacher = this.teacherRepository.findOne(teacherId);
-		List<Subject> subjects = this.subjectRepository.findAllByTeacherId(teacherId);
-		List<Subject> subjectsNotTaught = this.subjectRepository.findAllByTeacherIdIsNullOrTeacherIdIsNot(teacherId);
-		m.addAttribute("teacher", teacher);
-		m.addAttribute("subjects", subjects);
-		m.addAttribute("subjectsNotTaught", subjectsNotTaught);
-		return "teacher/addSubject_teacher";
-	}
-
-	@GetMapping("addSubject/{teacherId}/{subjectId}")
-	public String addSubject(@PathVariable long teacherId, @PathVariable long subjectId) {
-		Teacher teacher = this.teacherRepository.findOne(teacherId);
-		Subject subject = this.subjectRepository.findOne(subjectId);
-		subject.getTeacher().add(teacher);
-		this.subjectRepository.save(subject);
-		return "redirect:/teacher/addSubject/{teacherId}";
-	}
-*/
 
 	// Additional methods
 	private User getLoggedUser() {
