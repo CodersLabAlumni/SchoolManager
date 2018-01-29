e4e5ab59f3ada6fa4df381b972960847937f2b5a
przemyslaw <przemyslawkonik@gmail.com>
2018-01-29 17:26:15 +0100

    fix register/update user methods

--- src/main/java/pl/schoolmanager/controller/HomeController.java
+++ src/main/java/pl/schoolmanager/controller/HomeController.java
@@ -4,73 +4,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.schoolmanager.bean.SessionManager;
-import pl.schoolmanager.bean.role.Role;
-import pl.schoolmanager.bean.role.factory.RoleFactory;
 import pl.schoolmanager.entity.User;
-import pl.schoolmanager.entity.UserRole;
-import pl.schoolmanager.repository.UserRepository;
-import pl.schoolmanager.repository.UserRoleRepository;
-
-import javax.transaction.Transactional;
-import javax.validation.Valid;
+import pl.schoolmanager.service.user.UserService;
+import pl.schoolmanager.validator.user.NewUsernameValidator;
+import pl.schoolmanager.validator.user.UserValidation;
 
 @Controller("/")
 public class HomeController {
 
-	@Autowired
-	private UserRepository userRepo;
+    @Autowired
+    private SessionManager sessionManager;
 
-	@Autowired
-	private UserRoleRepository userRoleRepo;
+    @Autowired
+    private UserService userService;
 
-	@Autowired
-	private SessionManager sessionManager;
+    @Autowired
+    private UserValidation userValidation;
 
-	@Autowired
-	private RoleFactory roleFactory;
+    @GetMapping("/")
+    public String home() {
+        sessionManager.updateMessageValues();
+        return "home/home";
+    }
 
-	@GetMapping("/")
-	public String home() {
-		sessionManager.updateMessageValues();
-		return "home/home";
-	}
+    @GetMapping("login")
+    public String loginGet() {
+        return "home/login";
+    }
 
-	@GetMapping("login")
-	public String loginGet() {
-		return "home/login";
-	}
+    @GetMapping("register")
+    public String registerGet(Model m) {
+        m.addAttribute("user", new User());
+        return "home/register";
+    }
 
-	@GetMapping("register")
-	public String registerGet(Model m) {
-		m.addAttribute("user", new User());
-		return "home/register";
-	}
+    @PostMapping("register")
+    public String registerPost(@Validated(NewUsernameValidator.class) @ModelAttribute User user, BindingResult bindingResult) {
+        if (userValidation.validRegister(user, bindingResult).hasErrors()) {
+            return "home/register";
+        }
+        userService.register(user);
+        return "redirect:/login";
+    }
 
-	@PostMapping("register")
-	@Transactional
-	public String registerPost(@Valid @ModelAttribute User user, BindingResult bindingResult, Model m) {
-		if (bindingResult.hasErrors()) {
-			return "redirect:register";
-		}
-		if (!user.isPasswordCorrent(user.getConfirmPassword())) {
-			m.addAttribute("msg", "Please make sure that both passwords match!");
-			return "home/register";
-		}
-		UserRole userRole = roleFactory.get(Role.USER);
-		user.setEnabled(true);
-		userRole.setUser(user);
-		this.userRepo.save(user);
-		this.userRoleRepo.save(userRole);
-		return "redirect:/login";
-	}
-
-	@GetMapping("403")
-	public String accessDenied() {
-		return "errors/accessDenied";
-	}
+    @GetMapping("403")
+    public String accessDenied() {
+        return "errors/accessDenied";
+    }
 
 }
--- src/main/java/pl/schoolmanager/controller/UserController.java
+++ src/main/java/pl/schoolmanager/controller/UserController.java
@@ -1,137 +1,99 @@
 package pl.schoolmanager.controller;
 
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import pl.schoolmanager.bean.SessionManager;
+import org.springframework.web.bind.annotation.*;
 import pl.schoolmanager.entity.User;
-import pl.schoolmanager.entity.UserRole;
 import pl.schoolmanager.repository.UserRepository;
-import pl.schoolmanager.repository.UserRoleRepository;
-import pl.schoolmanager.validator.EditUsernameValidator;
-import pl.schoolmanager.validator.NewUsernameValidator;
+import pl.schoolmanager.service.user.UserService;
+import pl.schoolmanager.validator.user.EditUsernameValidator;
+import pl.schoolmanager.validator.user.NewUsernameValidator;
+import pl.schoolmanager.validator.user.UserPasswordValidator;
+import pl.schoolmanager.validator.user.UserValidation;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-	@Autowired
-	private UserRepository userRepo;
+    @Autowired
+    private UserRepository userRepo;
 
-	@Autowired
-	private UserRoleRepository userRoleRepo;
+    @Autowired
+    private UserService userService;
 
-	@GetMapping("/create")
-	public String createUser(Model m) {
-		m.addAttribute("user", new User());
-		return "user/new_user";
-	}
+    @Autowired
+    private UserValidation userValidation;
 
-	@PostMapping("/create")
-	public String createUserPost(@Validated(NewUsernameValidator.class) User user, BindingResult bindingResult, 
-								Model m) {
-		if (bindingResult.hasErrors()) {
-			return "redirect:/user/new_user";
-		} else {
-			UserRole userRole = new UserRole();
-			user.setEnabled(true);
-			userRole.setUsername(user.getUsername());
-			userRole.setUser(user);
-			userRole.setUserRole("ROLE_USER");
-			this.userRepo.save(user);
-			this.userRoleRepo.save(userRole);
-			return "redirect:/user/all";
-		}
-	}
-	
-	@GetMapping("/view/{userId}")
-	public String viewUser(Model m, @PathVariable long userId) {
-		User user = this.userRepo.findOne(userId);
-		m.addAttribute("user", user);
-		return "user/show_user";
-	}
+    @GetMapping("/create")
+    public String createUser(Model m) {
+        m.addAttribute("user", new User());
+        return "home/register";
+    }
 
-	@GetMapping("/update/{userId}")
-	public String updateUserRole(Model m, @PathVariable long userId) {
-		User user = this.userRepo.findOne(userId);
-		m.addAttribute("user", user);
-		HttpSession session = SessionManager.session();
-		session.setAttribute("password", user.getPassword());
-		return "user/edit_user";
-	}
+    @PostMapping("/create")
+    public String createUser(@Validated(NewUsernameValidator.class) User user, BindingResult bindingResult) {
+        if (userValidation.validRegister(user, bindingResult).hasErrors()) {
+            return "home/register";
+        }
+        userService.register(user);
+        return "redirect:/user/all";
+    }
 
-	@PostMapping("/update/{id}")
-	@Transactional
-	public String updateUserPost(@Validated(EditUsernameValidator.class) @ModelAttribute User user, 
-			BindingResult bindingResult, @PathVariable long id) {
-		if (bindingResult.hasErrors()) {
-			return "user/edit_user";
-		}
-		HttpSession session = SessionManager.session();
-		user.setPasswordEncrypted(session.getAttribute("password").toString());
-		session.setAttribute("password", null);
-		this.userRepo.save(user);
-		this.userRoleRepo.updateWithUsernameByUserId(id, user.getUsername());
-		return "redirect:/user/all";
-	}
-	
-	@GetMapping("/changepassword/{userId}")
-	public String changePassword(@PathVariable long userId, Model m) {
-		User user = userRepo.findOne(userId);
-		m.addAttribute("user", user);
-		return "user/change_pass";
-	}
-	
-	@PostMapping("/changepassword/{userId}")
-	public String changePasswordPost(@RequestParam("password1") String password1, Model m,
-									@RequestParam("password2") String password2, @PathVariable long userId) {
-		User user = userRepo.findOne(userId);
-		if (!password1.equals(password2)) {
-			m.addAttribute("msg", "Both passwords must match!");
-			m.addAttribute("user", user);
-			return "user/change_pass";
-		}
-		user.setPassword(password1);
-		userRepo.save(user);
-		return "redirect:/user/all";
-	}
+    @GetMapping("/view/{userId}")
+    public String viewUser(Model m, @PathVariable long userId) {
+        m.addAttribute("user", userService.user(userId));
+        return "user/show_user";
+    }
 
-	@GetMapping("/delete/{userId}")
-	public String deleteUser(@PathVariable long userId, Model m) {
-		User user = this.userRepo.findOne(userId);
-		m.addAttribute("user", user);
-		return "user/confirm_delete";
-	}
-	
-	@PostMapping("/delete/{userId}")
-	public String deleteUserPost(@PathVariable long userId, Model m) {
-		this.userRepo.delete(userId);
-		return "redirect:/user/all";
-		
-	}
+    @GetMapping("/update/{userId}")
+    public String updateUser(Model m, @PathVariable long userId) {
+        m.addAttribute("user", userService.user(userId));
+        return "user/edit_user";
+    }
 
-	@GetMapping("/all")
-	public String allUsers(Model m) {
-		return "user/all_users";
-	}
-	
-	@ModelAttribute("availableUsers")
-	public List<User> getUsers() {
-		return this.userRepo.findAll();
-	}
-	
+    @PostMapping("/update/{userId}")
+    public String updateUser(@Validated(EditUsernameValidator.class) @ModelAttribute User user, BindingResult bindingResult) {
+        if (userValidation.validEdit(user, bindingResult).hasErrors()) {
+            return "user/edit_user";
+        }
+        userService.update(user);
+        return "redirect:/user/all";
+    }
+
+    @GetMapping("/changepassword/{userId}")
+    public String changePassword(@PathVariable long userId, Model m) {
+        m.addAttribute("user", userService.user(userId));
+        return "user/change_pass";
+    }
+
+    @PostMapping("/changepassword/{userId}")
+    public String changePassword(@Validated(UserPasswordValidator.class) User user, BindingResult bindingResult, @PathVariable long userId) {
+        if (userValidation.validPassword(user, bindingResult).hasErrors()) {
+            return "user/change_pass";
+        }
+        userService.changePassword(userId, user.getPassword());
+        return "redirect:/user/all";
+    }
+
+    @GetMapping("/delete/{userId}")
+    public String deleteUser(@PathVariable long userId, Model m) {
+        m.addAttribute("user", userService.user(userId));
+        return "user/confirm_delete";
+    }
+
+    @PostMapping("/delete/{userId}")
+    public String deleteUser(@PathVariable long userId) {
+        userService.delete(userId);
+        return "redirect:/user/all";
+    }
+
+    @GetMapping("/all")
+    public String allUsers(Model m) {
+        m.addAttribute("availableUsers", userRepo.findAll());
+        return "user/all_users";
+    }
+
 }
